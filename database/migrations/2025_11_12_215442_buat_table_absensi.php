871239da6eac1ed43ca4a5f442fd5894927cd27a
Ugar <fubamstream@gmail.com>
2025-11-12 22:05:53 +0700

    buat tabel absesensi saja

---
+++ database/migrations/2025_11_12_215442_buat_table_absensi.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensi', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('id_karyawan');
+
+            $table->foreign('id_karyawan')
+                  ->references('id')
+                  ->on('karyawan')
+                  ->onDelete('cascade');
+                
+            $table->date('tanggal');
+            $table->time('waktu_masuk');
+            $table->time('waktu_keluar')->nullable();
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensi');
+    }
+};
